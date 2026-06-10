b29a1d1cc91d5269023f6640e5d4738f210907a2
Ryan Wilson <ryanwilson@duck.com>
2026-06-10 10:08:23 -0500

    Handle errors

--- ubunturecovery.sh
+++ ubunturecovery.sh
@@ -1,38 +1,97 @@
 #!/bin/bash
 
-set -e
-
 ROOT_PARTITION="/dev/nvme0n1p6"
 EFI_PARTITION="/dev/nvme0n1p1"
 MOUNT_POINT="/mnt"
 
+CURRENT_STEP="Starting"
+
+error_handler() {
+    EXIT_CODE=$?
+
+    echo
+    echo "======================================================"
+    echo "[ERROR] Script failed"
+    echo "Step: $CURRENT_STEP"
+    echo "Exit Code: $EXIT_CODE"
+    echo "======================================================"
+    echo
+
+    case "$CURRENT_STEP" in
+        "Mount Root")
+            echo "Try manually:"
+            echo "mount $ROOT_PARTITION $MOUNT_POINT"
+            ;;
+        "Mount EFI")
+            echo "Try manually:"
+            echo "mount $EFI_PARTITION $MOUNT_POINT/boot/efi"
+            ;;
+        "Bind Mounts")
+            echo "Try manually:"
+            echo "for i in /dev /dev/pts /proc /sys /run; do mount -B \$i $MOUNT_POINT\$i; done"
+            ;;
+        "GRUB Install")
+            echo "Enter chroot manually:"
+            echo "chroot $MOUNT_POINT"
+            echo
+            echo "Then run:"
+            echo "grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ubuntu --recheck"
+            echo "update-grub"
+            ;;
+        "Cleanup")
+            echo "Unmount manually:"
+            echo "for i in /run /sys /proc /dev/pts /dev; do umount $MOUNT_POINT\$i; done"
+            echo "umount $MOUNT_POINT/boot/efi"
+            echo "umount $MOUNT_POINT"
+            ;;
+    esac
+
+    exit $EXIT_CODE
+}
+
+trap error_handler ERR
+
+set -e
+
 echo "[+] Mounting root partition..."
+CURRENT_STEP="Mount Root"
 mount "$ROOT_PARTITION" "$MOUNT_POINT"
 
 echo "[+] Mounting EFI partition..."
+CURRENT_STEP="Mount EFI"
 mount "$EFI_PARTITION" "$MOUNT_POINT/boot/efi"
 
-echo "[+] Binding system directories..."
+echo "[+] Creating bind mounts..."
+CURRENT_STEP="Bind Mounts"
 for i in /dev /dev/pts /proc /sys /run; do
     mount -B "$i" "$MOUNT_POINT$i"
 done
 
-echo "[+] Reinstalling GRUB..."
-chroot "$MOUNT_POINT" /bin/bash -c "
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ubuntu --recheck
-update-grub
-"
+echo "[+] Installing GRUB..."
+CURRENT_STEP="GRUB Install"
 
-echo "[+] Unmounting bind mounts..."
+chroot "$MOUNT_POINT" /bin/bash -c '
+set -e
+grub-install --target=x86_64-efi \
+             --efi-directory=/boot/efi \
+             --bootloader-id=ubuntu \
+             --recheck
+
+update-grub
+'
+
+echo "[+] Cleaning up..."
+CURRENT_STEP="Cleanup"
+
 for i in /run /sys /proc /dev/pts /dev; do
     umount "$MOUNT_POINT$i"
 done
 
-echo "[+] Unmounting EFI..."
 umount "$MOUNT_POINT/boot/efi"
-
-echo "[+] Unmounting root..."
 umount "$MOUNT_POINT"
 
-echo "[+] Rebooting..."
+echo
+echo "[SUCCESS] GRUB reinstall completed."
+echo "[+] Rebooting in 5 seconds..."
+sleep 5
 reboot
